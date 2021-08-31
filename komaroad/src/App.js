f270fd18a697f3680c6bf644df99375652b1efa1
Ivan <hammer.99.ivan@gmail.com>
2021-08-31 16:36:19 +0700

    antd

---
+++ komaroad/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Main from './Components/Main';
+
+
+function App() {
+  return (
+    <>
+      <Main/>
+    </>
+  );
+}
+
+export default App;
---
+++ komaroad/src/Components/Main.jsx
@@ -0,0 +1,11 @@
+
+
+function Main() {
+  return (
+    <>
+
+    </>
+  );
+}
+
+export default Main;
